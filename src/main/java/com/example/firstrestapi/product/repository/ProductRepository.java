06c2d178857a82b7c19954837935c92796c8714d
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-04 23:20:05 +0100

    I have added a Temporary Hash Map in the ProductRepository as a placeholder for my database. I have also added the method to save the product and also set its id number according to its index number in the HashMap.

--- src/main/java/com/example/firstrestapi/product/repository/ProductRepository.java
+++ src/main/java/com/example/firstrestapi/product/repository/ProductRepository.java
@@ -8,6 +8,19 @@ import java.util.Map;
 
 @Repository
 public class ProductRepository {
-
+    protected final Map<Long,Product> map = new HashMap<>();
+    protected long counter = 1; /* This is meant to count the iteration of the items in the hashmap*/
+    // This is meant to save the product.
+    public Product save(Product entity){
+        setId(entity);
+        return entity;
+    }
+    // This is a method meant to set the product Id to the index of the item in the Hashmap.
+    private Product setId(Product entity){
+        entity.setId(counter);
+        map.put(counter,entity);
+        counter++;
+        return entity;
+    }
 
 }
