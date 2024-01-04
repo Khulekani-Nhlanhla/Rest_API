5b1a16a45b905a5c8e580cffbdac8468684cec8c
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-04 23:45:23 +0100

    I have added Methods in the ProductMapper class to map object data from one object to another.

--- src/main/java/com/example/firstrestapi/product/support/ProductMapper.java
+++ src/main/java/com/example/firstrestapi/product/support/ProductMapper.java
@@ -1,11 +1,19 @@
 package com.example.firstrestapi.product.support;
 
+import com.example.firstrestapi.product.api.ProductController;
 import com.example.firstrestapi.product.api.request.ProductRequest;
 import com.example.firstrestapi.product.api.response.ProductResponse;
 import com.example.firstrestapi.product.domain.Product;
 import org.springframework.stereotype.Component;
 
-
+// The Mapper is for the mapping of objects and their data.
+@Component
 public class ProductMapper {
+    public Product toProduct(ProductRequest productRequest){
+        return new Product(productRequest.getName());
+    }
 
+    public ProductResponse toProductResponse(Product product){
+        return new ProductResponse(product.getId(),product.getName());
+    }
 }
