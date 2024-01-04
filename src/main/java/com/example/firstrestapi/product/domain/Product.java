45fd3115607b9c3cabc3a42187e4fdb0b5cf4588
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-04 23:10:49 +0100

    I have added the getters and setters of the product id and name in the Product Class. I have also created a Product Constructor.

--- src/main/java/com/example/firstrestapi/product/domain/Product.java
+++ src/main/java/com/example/firstrestapi/product/domain/Product.java
@@ -2,4 +2,20 @@ package com.example.firstrestapi.product.domain;
 
 public class Product {
 
+    private Long id;
+
+    private String name;
+//    This a method meant to assign the name of the product
+    public Product(String name){this.name = name;}
+
+    // This method is responsible for retrieving the product id
+    public Long getId(){return id;}
+    // This method is for retrieving the product name.
+    public String getName() {return name;}
+    // This method is for setting or changing the product id
+    public void setId(Long id){this.id = id;}
+    // This method is for setting or changing the product name
+    public void setName(String name){
+        this.name = name;
+    }
 }
