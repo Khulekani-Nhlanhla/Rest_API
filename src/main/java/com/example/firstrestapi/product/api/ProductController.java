03b1c5e0970eb4b1ea7519d97e0571e04d27bc21
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-05 00:03:08 +0100

    I have made a Response method in the ProductController class.

--- src/main/java/com/example/firstrestapi/product/api/ProductController.java
+++ src/main/java/com/example/firstrestapi/product/api/ProductController.java
@@ -7,7 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+// This is responsible for sending and handling http requests
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final ProductService productService;
+    public ProductController(ProductService productService){this.productService = productService;}
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> create(@RequestBody ProductRequest productRequest){
+        ProductResponse productResponse = productService.create(productRequest);
+        return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+    }
 
 }
