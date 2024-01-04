8ef4b2a3705a9389607e520696dde1184788b1aa
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-04 23:34:32 +0100

    I have added Constructors for the ProductRequest and ProductResponse Classes with a getName method for each.

--- src/main/java/com/example/firstrestapi/product/api/request/ProductRequest.java
+++ src/main/java/com/example/firstrestapi/product/api/request/ProductRequest.java
@@ -3,5 +3,11 @@ package com.example.firstrestapi.product.api.request;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public class ProductRequest {
+    private final String name;
 
+    // This a method meant to specify the constructor meant to be used during deserialization specifically product name.
+    @JsonCreator
+    public ProductRequest(String name){this.name = name;}
+
+    public String getName(){return name;}
 }
