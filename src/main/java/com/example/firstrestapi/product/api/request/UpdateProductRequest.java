c8acf874ffaaf3e9549d941e54089444246d601d
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-05 03:27:22 +0100

    In made a UpdateProductRequest class to store the put method.

---
+++ src/main/java/com/example/firstrestapi/product/api/request/UpdateProductRequest.java
@@ -0,0 +1,17 @@
+package com.example.firstrestapi.product.api.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.hibernate.sql.Update;
+
+public class UpdateProductRequest extends ProductRequest{
+    private final Long id;
+
+    @JsonCreator
+    public UpdateProductRequest(String name, Long id){
+        super(name);
+        this.id = id;
+    }
+    public Long getId(){
+        return id;
+    }
+}
